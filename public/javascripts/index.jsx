102aeb3c56ac33cce0f0684753a1619cf0f65c3e
Yingqian Mei <cece@Yingqians-MacBook-Air.local>
2017-12-04 22:59:47 -0800

    second commit

---
+++ public/javascripts/index.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './app/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    window.__root_container = document.getElementById('root');
+    ReactDOM.render(<Root/>, window.__root_container);
+
+});
