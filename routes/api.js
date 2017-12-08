5abb521db13e06032100df2d7659d2d212bc39b8
cecemei <yingqian.mei@gmail.com>
2017-12-07 21:57:50 -0800

    add error page and api router

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+
+
+const express = require('express');
+const router = express.Router();
+
+/* GET api . */
+router.get('/', function(req, res, next) {
+   res.send({ title: 'API service page.' });
+
+});
+
+
+router.use(function(req, res, next) {
+    // render the error page
+    res.status(404);
+    res.send({"error reason":"no such api"});
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 
 
 const express = require('express');
-
+const api = require('./routes/api');
 const port = 3001 || process.env.PORT;
 const app = express();
 
-//app.set('views', __dirname + '/dev');
-//app.set('view engine', 'mustache');
-app.use(express.static(__dirname + '/dev'));
+//serving RESTful API
+app.use('/api', api);
+//serving static page
+app.use('/', express.static(__dirname + '/dev'));
 
 app.listen(port, function onStart(err) {
   if (err) {
@@ -16,3 +17,19 @@ app.listen(port, function onStart(err) {
   console.log('Listening on http://localhost:' + port);
   console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
 });
+
+app.use(function(req, res, next) {
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+ });
+
+ app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.sendFile('dev/error.html', {root: __dirname });
+  });
